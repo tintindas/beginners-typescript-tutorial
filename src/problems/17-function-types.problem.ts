566ca4e2658fd2a75d9078586c4cea8a9dd513a4
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-10-05 01:22:05 +0200

    solve exercise 17

--- src/problems/17-function-types.problem.ts
+++ src/problems/17-function-types.problem.ts
@@ -1,9 +1,9 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from "../helpers/type-utils";
 
 /**
  * How do we type onFocusChange?
  */
-const addListener = (onFocusChange: unknown) => {
+const addListener = (onFocusChange: (isFocused: boolean) => void) => {
   window.addEventListener("focus", () => {
     onFocusChange(true);
   });
